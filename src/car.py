5620de6e3ef3d74cc3d11d66dda42406afd1fb21
MatureKid <157224899+PeafulLeopold@users.noreply.github.com>
2025-02-28 01:17:14 +1000

    Update car.py

--- src/car.py
+++ src/car.py
@@ -10,24 +10,30 @@ class Car:
         self.control_scheme = control_scheme  # Схема управления
 
     def set_move_bounds(self, road_rect):
-        """Установка границ движения на основе дороги"""
-        self.move_bounds = road_rect.inflate(100, 100)  # + отступы от краев
+        self.move_bounds = road_rect.inflate(100, 100)
 
     def update(self, keys):
         # Движение в зависимости от схемы управления
         if self.control_scheme == "arrows":
-            if keys[pygame.K_UP]: self.rect.y -= self.speed
-            if keys[pygame.K_DOWN]: self.rect.y += self.speed
-            if keys[pygame.K_LEFT]: self.rect.x -= self.speed * 1.5
-            if keys[pygame.K_RIGHT]: self.rect.x += self.speed * 1.5
+            if keys[pygame.K_UP]: 
+                self.rect.y -= self.speed
+            if keys[pygame.K_DOWN]:
+                self.rect.y += self.speed
+            if keys[pygame.K_LEFT]: 
+                self.rect.x -= self.speed * 1.5
+            if keys[pygame.K_RIGHT]: 
+                self.rect.x += self.speed * 1.5
         elif self.control_scheme == "wasd":
-            if keys[pygame.K_w]: self.rect.y -= self.speed
-            if keys[pygame.K_s]: self.rect.y += self.speed
-            if keys[pygame.K_a]: self.rect.x -= self.speed * 1.5
-            if keys[pygame.K_d]: self.rect.x += self.speed * 1.5
-
-        # Ограничение движения
-        self.rect.clamp_ip(self.move_bounds)  # Жесткие границы
+            if keys[pygame.K_w]: 
+                self.rect.y -= self.speed
+            if keys[pygame.K_s]: 
+                self.rect.y += self.speed
+            if keys[pygame.K_a]: 
+                self.rect.x -= self.speed * 1.5
+            if keys[pygame.K_d]: 
+                self.rect.x += self.speed * 1.5
+        
+        self.rect.clamp_ip(self.move_bounds)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
